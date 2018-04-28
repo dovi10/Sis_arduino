d072e7b4ed27e26d89d3601f015a8a6e530cecdf
unknown <sdovi10@gmail.com>
2018-04-28 23:04:05 +0300

    Added wifi connection

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,37 +1,34 @@
-/*
-Blink
-
-Turns an LED on for one second, then off for one second, repeatedly.
-
-Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-the correct LED pin independent of which board is used.
-If you want to know what pin the on-board LED is connected to on your Arduino
-model, check the Technical Specs of your board at:
-https://www.arduino.cc/en/Main/Products
-
-modified 8 May 2014
-by Scott Fitzgerald
-modified 2 Sep 2016
-by Arturo Guadalupi
-modified 8 Sep 2016
-by Colby Newman
-
-This example code is in the public domain.
-
-http://www.arduino.cc/en/Tutorial/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
+int ledPin = 13; 
+int state = 0;
+int flag = 0; 
+ 
 void setup() {
-	// initialize digital pin LED_BUILTIN as an output.
-	pinMode(LED_BUILTIN, OUTPUT);
+ pinMode(ledPin, OUTPUT);
+ digitalWrite(ledPin, LOW);
+ 
+ Serial.begin(9600); // Default connection rate for my BT module
 }
-
-// the loop function runs over and over again forever
+ 
 void loop() {
-	digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-	delay(1000);                       // wait for a second
-	digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-	delay(1000);                       // wait for a second
+
+ if(Serial.available() > 0){
+  state = Serial.read();
+  flag=0;
+ }
+
+ if (state == '0') {
+  digitalWrite(ledPin, LOW);
+  if(flag == 0){
+   Serial.println("LED: off");
+   flag = 1;
+  }
+ }
+
+ else if (state == '1') {
+  digitalWrite(ledPin, HIGH);
+  if(flag == 0){
+   Serial.println("LED: on");
+   flag = 1;
+  }
+ }
 }
