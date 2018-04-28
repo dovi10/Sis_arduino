1444cafedd1de9c4ba51b9c24c975303d163cbb4
unknown <sdovi10@gmail.com>
2018-04-28 23:19:44 +0300

    Added humidity sensor

--- arduino_code.ino
+++ arduino_code.ino
@@ -3,6 +3,10 @@ int state = 0;
 int flag = 0;
 int Grove_Water_Sensor =  8;
 
+int Humidity_sensor_Pin = A0;
+int Humidity_sensor_Value = 0;
+int Humidity_percent = 0;
+
 void setup() {
   pinMode(ledPin, OUTPUT);
 //  digitalWrite(ledPin, LOW);
@@ -13,14 +17,36 @@ void setup() {
   
 void loop() {
 
+  Humidity_sensor_Value = analogRead(Humidity_sensor_Pin);
+  Humidity_percent = convertToPercent(Humidity_sensor_Value);
+  printValuesToSerial();
+
   if (digitalRead(Grove_Water_Sensor) == LOW) {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("There is Water!!!\n");
+    digitalWrite(ledPin, LOW);
+    Serial.println("It's dry!!!\n");
     delay(2000);
   }
   else {
-    digitalWrite(ledPin, LOW);
-    Serial.println("Its dry!!!\n");
+    digitalWrite(ledPin, HIGH);
+    Serial.println("It's wet!!!\n");
     delay(2000);
   }
 }
+
+
+int convertToPercent(int value)
+{
+  int percentValue = 0;
+  percentValue = map(value, 1023, 465, 0, 100);
+  return percentValue;
+}
+
+void printValuesToSerial()
+{
+  Serial.print("\n\nHumidity: ");
+  Serial.print(Humidity_sensor_Value);
+  Serial.print(" Percent: ");
+  Serial.print(Humidity_percent);
+  Serial.println(" %");
+}
+
