f45aaedc68e49c3b4885f4a6146b31f3bbba2db6
unknown <sdovi10@gmail.com>
2018-04-28 23:24:30 +0300

    Added קemergency buzzer

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,3 +1,5 @@
+#include "pitches.h"  //add note library
+
 int ledPin = 13;
 int state = 0;
 int flag = 0;
@@ -6,6 +8,16 @@ int Grove_Water_Sensor =  8;
 int Humidity_sensor_Pin = A0;
 int Humidity_sensor_Value = 0;
 int Humidity_percent = 0;
+
+//notes in the melody
+int melody[]={NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+
+//digital pin 12 has a button attached to it. Give it an name
+int buzzerPin= 7;
+
+//note durations. 4=quarter note / 8=eighth note
+int noteDurations[]={4, 8, 8, 4, 4, 4, 4, 4};
+
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -16,12 +28,11 @@ void setup() {
 }
   
 void loop() {
-
   Humidity_sensor_Value = analogRead(Humidity_sensor_Pin);
   Humidity_percent = convertToPercent(Humidity_sensor_Value);
   printValuesToSerial();
 
-  if (digitalRead(Grove_Water_Sensor) == LOW) {
+  if (digitalRead(Grove_Water_Sensor) == HIGH) {
     digitalWrite(ledPin, LOW);
     Serial.println("It's dry!!!\n");
     delay(2000);
@@ -29,6 +40,7 @@ void loop() {
   else {
     digitalWrite(ledPin, HIGH);
     Serial.println("It's wet!!!\n");
+    playBuzzer();
     delay(2000);
   }
 }
@@ -45,8 +57,27 @@ void printValuesToSerial()
 {
   Serial.print("\n\nHumidity: ");
   Serial.print(Humidity_sensor_Value);
-  Serial.print(" Percent: ");
+  Serial.print(" Percent: "); 
   Serial.print(Humidity_percent);
   Serial.println(" %");
+}
+
+
+void playBuzzer(){
+    //iterate over the notes of the melody
+    for (int thisNote=0; thisNote <8; thisNote++){
+
+      //to calculate the note duration, take one second. Divided by the note type
+      int noteDuration = 1000 / noteDurations [thisNote];
+      tone(buzzerPin, melody [thisNote], noteDuration);
+
+      //to distinguish the notes, set a minimum time between them
+      //the note's duration +30% seems to work well
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+
+      //stop the tone playing
+      noTone(buzzerPin);
+    }
 }
 
